bd80c2ffcdda1bacc59178ed0c5d7cc22443718c
Maged Adel <maged@compucorp.co.uk>
2017-06-15 23:28:10 +0200

    Rabish and unorganized code to prove that we can retrieve images, js and css for a page

--- action.php
+++ action.php
@@ -25,15 +25,36 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 // $output contains the output string
 $output = curl_exec($ch);
 $page_markup = phpQuery::newDocumentHTML($output, $charset = 'utf-8');
-$images = pq('img', $page_markup);
-print "Lengths : {$images->length}";
+$images = pq("img", $page_markup);
+$js = pq("script", $page_markup);
+$css = pq("link[rel='stylesheet']", $page_markup);
+$images_array = [];
+$js_array = [];
+$css_array = [];
 
-foreach($images as $something) {
-  var_dump(pq($something)->attr("src"));
+foreach($images as $image) {
+  $images_array[] = pq($image)->attr("src");
 }
 
+foreach($js as $js_item) {
+  if(!empty(pq($js_item)->attr("src"))) {
+    $js_array[] = pq($js_item)->attr("src");
+  }
+}
 
-var_dump(curl_getinfo($ch)) . '<br/>';
+foreach($css as $css_item) {
+  if(!empty(pq($css_item)->attr("href"))) {
+    $css_array[] = pq($css_item)->attr("href");
+  }
+}
+print "<h3>IMAGES</h3>";
+var_dump($images_array);
+print "<h3>JS</h3>";
+var_dump($js_array);
+print "<h3>CSS</h3>";
+var_dump($css_array);
+
+
 echo curl_errno($ch) . '<br/>';
 echo curl_error($ch) . '<br/>';
 
