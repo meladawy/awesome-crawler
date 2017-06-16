236aedbebd6c7e2e068ea8b467cd9d60d7fbb569
Maged Adel <maged@compucorp.co.uk>
2017-06-17 00:36:26 +0200

    Use Curl and organize the code a little bit more

--- app/controllers/ProcessController.php
+++ app/controllers/ProcessController.php
@@ -1,6 +1,7 @@
 <?php
 
 use \phpQuery;
+use \Curl\Curl;
 
 /**
  * Process Controller that handle the homepage for.
@@ -8,69 +9,81 @@ use \phpQuery;
 class ProcessController extends Controller
 {
   // I use this private variables for redirect.
-  private $home_page =  FRONT_PATH ;
+  private $home_page =  FRONT_PATH;
+  private $curl;
+  private $website = '';
+  private $output = '';
+
+  function __construct () {
+    $this->curl = new Curl();
+    $this->curl->setOpt(CURLOPT_FOLLOWLOCATION, TRUE);
+  }
 
   /**
    * Process handling function
    */
-  public function index() {
-    $website_url = !empty($_POST['url']) ? $_POST['url'] : "";
+   public function index() {
+     // Initialize variables
+     $this->website = !empty($_POST['url']) ? $_POST['url'] : "";
 
-    print($website_url);
+     if(empty($this->website)) {
+       echo "Something went wrong";
+       return;
+     }
 
-    // create curl resource
-    $ch = curl_init();
+     $this->curl->get($this->website);
 
-    // set url
-    curl_setopt($ch, CURLOPT_URL, $website_url);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    //return the transfer as a string
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+     if ($this->curl->error) {
+       echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
+       return;
+     }
 
-    // $output contains the output string
-    $output = curl_exec($ch);
-    $page_markup = phpQuery::newDocumentHTML($output, $charset = 'utf-8');
-    $images = pq("img", $page_markup);
-    $js = pq("script", $page_markup);
-    $css = pq("link[rel='stylesheet']", $page_markup);
-    $images_array = [];
-    $js_array = [];
-    $css_array = [];
+     // $output contains the output string
+     $this->output = $this->curl->response;
 
-    foreach($images as $image) {
-      $images_array[] = pq($image)->attr("src");
-    }
+     var_dump($this->getImages());
+     var_dump($this->getJS());
+     var_dump($this->getCSS());
 
-    foreach($js as $js_item) {
-      if(!empty(pq($js_item)->attr("src"))) {
-        $js_array[] = pq($js_item)->attr("src");
-      }
-    }
+   }
 
-    foreach($css as $css_item) {
-      if(!empty(pq($css_item)->attr("href"))) {
-        $css_array[] = pq($css_item)->attr("href");
-      }
-    }
-    print "<h3>IMAGES</h3>";
-    var_dump($images_array);
-    print "<h3>JS</h3>";
-    var_dump($js_array);
-    print "<h3>CSS</h3>";
-    var_dump($css_array);
+   function getImages() {
+     $images_array = [];
+     $page_markup = phpQuery::newDocumentHTML($this->output, $charset = 'utf-8');
+     $images = pq("img", $page_markup);
 
+     foreach($images as $image) {
+       $images_array[] = pq($image)->attr("src");
+     }
 
-    echo curl_errno($ch) . '<br/>';
-    echo curl_error($ch) . '<br/>';
+     return $images_array;
+   }
 
+   function getJS() {
+     $js_array = [];
+     $page_markup = phpQuery::newDocumentHTML($this->output, $charset = 'utf-8');
+     $js = pq("script", $page_markup);
 
-    // close curl resource to free up system resources
-    curl_close($ch);
+     foreach($js as $js_item) {
+       if(!empty(pq($js_item)->attr("src"))) {
+         $js_array[] = pq($js_item)->attr("src");
+       }
+     }
 
-    var_dump($output);
+     return $js_array;
+   }
 
-  }
+   function getCSS() {
+     $css_array = [];
+     $page_markup = phpQuery::newDocumentHTML($this->output, $charset = 'utf-8');
+     $css = pq("link[rel='stylesheet']", $page_markup);
+
+     foreach($css as $css_item) {
+       if(!empty(pq($css_item)->attr("href"))) {
+         $css_array[] = pq($css_item)->attr("href");
+       }
+     }
+
+     return $css_array;
+   }
 }
